18fd4f138cc7d300227fa33e9bf55caf034ba284
Dominic Flocco <64973207+dominicflocco@users.noreply.github.com>
2023-05-17 14:53:11 -0400

    Update codes

--- final/schrodingerSolverFT.m
+++ final/schrodingerSolverFT.m
@@ -1,33 +1,55 @@
 
+%% AMSC 661 Final Exam Problem 2
+% Method of Fourier Transforms
+% Author: Dominic Flocco 
+% Date: May 17, 2023
+
+
 function schrodingerSolverFT
-    % define parameters 
+    %% Initialization
+    % Input parameters 
     k0 = 10; s0 = 0.1;
-    Tmax = 0.4;
-    nPts = 4096; 
-    x = linspace(-20,20,nPts);
+    Tmax = 0.4; L = 20; nPts = 256; 
     
-    dx = 40/nPts;
+    dx = 2*L/nPts;
     dt = 10^(-4);
     tSteps = floor(Tmax/dt);
+    % Initialize space and time intervals
+    tSteps = floor(Tmax/dt);
     t = linspace(0,Tmax,tSteps);
-    L = 40;
-    u = zeros(nPts, tSteps);
+    x = linspace(-L,L,nPts);
+
+    % Initialize solution array 
+    u = zeros(nPts, tSteps); 
+    
+    % Compute solution at time 0
     u(:,1) = initial(x,s0,k0)';
+    % Fourier Transform at time 0
     f0 = fftshift(fft(u(:,1)'));
-    freqs = 2*pi*[-nPts/2 : nPts/2 - 1]/L;
 
-
+    % Initialize frequencies
+    freqs = pi*[-nPts/2 : nPts/2 - 1]/L;
+    
+    %% Time March
     for k =1:tSteps
+        % Compute Fourier mode at time t(k)
         ft = f0.*exp(-1j*freqs.^2*t(k)/2);
+        % Perform IFT on Fourier mode to obtain solution 
         u(:,k) = ifft(ifftshift(ft));
-
     end
-    ex = exact(x,0,s0,k0);
+    
+    % Compute integral of probability density over interval at time 0
+    density = trapz(abs(u(:,1)).^2,x);
+    fprintf('Density integral at time t = %d: %f\n', 0,abs(density));
+    
+    %% Plot Solution 
     figure;
     subplot(3,2,1);
     hold on;
+    ex = exact(x,0,s0,k0);
     plot(x, abs(u(:,1)),'DisplayName','Numerical','LineWidth', 1,'Color','r');
     plot(x,abs(ex), 'DisplayName','Exact','LineWidth', 1,'Color','b');
+    
     title('$$t=0$$', 'interpreter','latex','Fontsize',18);
     legend('interpreter','latex', 'FontSize',12)
     axis([-20,20,0,2]);
@@ -46,6 +68,12 @@ function schrodingerSolverFT
         t = Tmax*(i/5);
         n = floor(t/dt);
         ex = exact(x,t,s0,k0);
+
+        % Compute integral of probability density over interval
+        density = trapz(abs(u(:,n)).^2,x);
+        fprintf('Density integral at time t = %.2f: %f\n', t,abs(density));
+        
+        % Plot solution 
         hold on;
         plot(x, abs(u(:,n)),'DisplayName','Numerical','LineWidth', 1,'Color','r');
         plot(x,abs(ex), 'DisplayName','Exact','LineWidth', 1,'Color','b');
@@ -57,10 +85,9 @@ function schrodingerSolverFT
         ylabel('$$|\psi(x,t)|$$','FontSize',14,'interpreter','latex')
         hold off;
     end
-
-    sgtitle('Solution to Schr\"{o}dinger Eq. using FT method with $$N_x = 4096$$','interpreter','latex','Fontsize',22,'FontWeight','Bold');
+    sgtitle('Solution to Schr\"{o}dinger Eq. using FT method with $$N_x = 256$$','interpreter','latex','Fontsize',22,'FontWeight','Bold');
 end
-
+%% Compute Exact Solution
 function sol = exact(x,t,s0,k0)
     A = (2*pi*s0^2)^(-1/4);
     aux = 1 + 1j*t/(2*s0^2);
@@ -68,6 +95,7 @@ function sol = exact(x,t,s0,k0)
     denom = 4*s0^2*aux;
     sol = A*exp(-num/denom)/sqrt(aux);
 end
+%% Compute Initial Condition
 function packet = initial(x,s0,k0)
     A = (2*pi*s0^2)^(-1/4);
     packet = A*exp(-x.^2/(4*s0^2) +1j*k0*x);
--- final/schrodingerSolverMOL.m
+++ final/schrodingerSolverMOL.m
@@ -1,38 +1,46 @@
-%% 
+%% AMSC 661 Final Exam Problem 2
+% Method of Lines (D0^2+RK4)
+% Author: Dominic Flocco 
+% Date: May 17, 2023
 
 function schrodingerSolverMOL
-    % define parameters 
+    %% Initialization 
+    % Input parameters
     k0 = 10; s0 = 0.1;
-    Tmax = 0.4;
-    nPts = 256; 
+    Tmax = 0.4; nPts = 256; 
     x = linspace(-20,20,nPts);
     dx = 40/nPts;
     dt = 10^(-4);
     tSteps = floor(Tmax/dt);
     
-    % Define central difference matrix 
+    %% Define Central Difference Scheme
     e = ones(nPts,1);
     A = spdiags([e -2*e e],-1:1, nPts,nPts);
     A(nPts,1) = 1; A(1,nPts) = 1;
     A = -(1j/(2*dx^2))*A;
+    % Initialize Solution
     u = zeros(nPts, tSteps);
     u(:,1) = initial(x,s0,k0)';
+    %% Time March
     for k = 1:tSteps-1
         % Runge Kutta 4th order stages
         k1 = A*(u(:,k)); 
         k2 = A*(u(:,k) + 0.5*dt*k1);
         k3 = A*(u(:,k) + 0.5*dt*k2); 
         k4 = A*(u(:,k) + dt*k3); 
+        % Compute Solution 
         u(:,k+1) = u(:,k) + dt*(k1+2*k2+2*k3+k4)/6;
     end
     
+    % Compute integral of probability density over interval at time 0
+    density = trapz(abs(u(:,1)).^2,x);
+    fprintf('Density integral at time t = %d: %f\n', 0,abs(density));
     
-    ex = exact(x,0,s0,k0);
-
-
+    %% Plot Solution
     figure;
     subplot(3,2,1);
     hold on;
+    ex = exact(x,0,s0,k0);
     plot(x, abs(u(:,1)),'DisplayName','Numerical','LineWidth', 1,'Color','r');
     plot(x,abs(ex), 'DisplayName','Exact','LineWidth', 1,'Color','b');
     title('$$t=0$$', 'interpreter','latex','Fontsize',18);
@@ -53,6 +61,11 @@ function schrodingerSolverMOL
         t = Tmax*(i/5);
         n = floor(t/dt);
         ex = exact(x,t,s0,k0);
+
+        % Compute integral of probability density over interval
+        density = trapz(abs(u(:,n)).^2,x);
+        fprintf('Density integral at time t = %.2f: %f\n', t,abs(density));
+
         hold on;
         plot(x, abs(u(:,n)),'DisplayName','Numerical','LineWidth', 1,'Color','r');
         plot(x,abs(ex), 'DisplayName','Exact','LineWidth', 1,'Color','b');
@@ -64,23 +77,18 @@ function schrodingerSolverMOL
         ylabel('$$|\psi(x,t)|$$','FontSize',14,'interpreter','latex')
         hold off;
     end
-
     sgtitle('Solution to Schr\"{o}dinger Eq. using $$D_0^2$$-RK4 with $$N_x = 256$$','interpreter','latex','Fontsize',22,'FontWeight','Bold');
 end
-
-function packet = initial(x,s0,k0)
-    A = (2*pi*s0^2)^(-1/4);
-    packet = A*exp(-x.^2/(4*s0^2) +1j*k0*x);
-end
-function f = rhs(u,dx)
-    ujp1 = circshift(u,[0, -1]);
-    ujm1 = circshift(u,[0, 1]);
-    f = (0.5*1j/dx^2)*(ujp1+ujm1 - 2*u);
-end
+%% Compute Exact Solution
 function sol = exact(x,t,s0,k0)
     A = (2*pi*s0^2)^(-1/4);
     aux = 1 + 1j*t/(2*s0^2);
     num = x.^2 - 4j*s0^2*k0*x +2j*s0^2*k0^2*t; 
     denom = 4*s0^2*aux;
     sol = A*exp(-num/denom)/sqrt(aux);
+end
+%% Compute Initial Condition
+function packet = initial(x,s0,k0)
+    A = (2*pi*s0^2)^(-1/4);
+    packet = A*exp(-x.^2/(4*s0^2) +1j*k0*x);
 end
